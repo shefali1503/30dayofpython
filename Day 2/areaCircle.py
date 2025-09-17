0e6d7350b36495108d4a91034ba9633618ff5cdb
shefali1503 <shefalisharmaa4455@gmail.com>
2025-09-17 22:23:44 +0530

    Day 2

---
+++ Day 2/areaCircle.py
@@ -0,0 +1,6 @@
+radius = float(input("Enter radius: "))
+area = 3.14159 * radius * radius
+circumference = 2 * 3.14159 * radius
+
+print("Area of Circle:", area)
+print("Circumference:", circumference)
