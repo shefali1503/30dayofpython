685d98f874e1cc03f7de8aa931b21e37c526b8c9
shefali1503 <shefalisharmaa4455@gmail.com>
2025-09-21 23:40:18 +0530

    Day 5

---
+++ Day 5/acessingelemnts.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "cherry", "mango"]
+
+print(fruits[0])   # apple (first element, index starts from 0)
+print(fruits[-1])  # mango (last element, negative index)
+print(fruits[1:3]) # ['banana', 'cherry'] (slicing)
---
+++ Day 5/creatinglist.py
@@ -0,0 +1,9 @@
+# Empty list
+my_list = []
+
+# List with values
+numbers = [1, 2, 3, 4, 5]
+fruits = ["apple", "banana", "cherry"]
+
+# Mixed data types
+mixed = [10, "hello", 3.14, True]
