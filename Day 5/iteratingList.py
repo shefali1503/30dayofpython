e84463a10da7eab4c02a6265c47681b7443b78b4
shefali1503 <shefalisharmaa4455@gmail.com>
2025-09-21 23:43:12 +0530

    Day 5

---
+++ Day 5/iteratingList.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+
+# Using for loop
+for fruit in fruits:
+    print(fruit)
+
+# Using while loop
+i = 0
+while i < len(fruits):
+    print(fruits[i])
+    i += 1
