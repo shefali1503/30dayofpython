19c06acf6024d6a1cd58cb7bf30988cde28f0e69
shefali1503 <shefalisharmaa4455@gmail.com>
2025-09-21 23:41:19 +0530

    Day 5

---
+++ Day 5/modifyinglists.py
@@ -0,0 +1,5 @@
+fruits[1] = "blueberry"   # Change element
+fruits.append("orange")   # Add at end
+fruits.insert(2, "kiwi")  # Insert at position
+fruits.remove("apple")    # Remove by value
+fruits.pop(1)             # Remove by index
