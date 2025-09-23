a41d1620410a532a03c83fe6bd7972b2ed0c9c00
shefali1503 <shefalisharmaa4455@gmail.com>
2025-09-23 22:03:01 +0530

    Day 6

---
+++ Day 6/creatingTuple.py
@@ -0,0 +1,11 @@
+# Create and access tuple elements
+
+# Creating a tuple
+my_tuple = (10, 20, 30, 40, 50)
+
+# Accessing elements
+print("First element:", my_tuple[0])
+print("Last element:", my_tuple[-1])
+
+# Slicing
+print("Tuple slice:", my_tuple[1:4])
